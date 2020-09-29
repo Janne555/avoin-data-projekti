b46052962213e2193751f8bb84d72ae83575f4a7
schatze <oskar@schatz.fi>
2020-09-29 12:53:45 -0400

    Velka backend

--- backend/index.js
+++ backend/index.js
@@ -63,6 +63,7 @@ app.post("/todo", async (req, res) => {
   }
 })
 
+
 // Example:
 // curl -X POST -d '{"name": "test", "done": true}' -H "Content-type: application/json"  localhost:4000/todo
 
@@ -72,6 +73,38 @@ app.get("/todo", async (req, res) => {
   } catch (error) {
     console.error(error)
     res.status(500).send("failed to get todos")
+  }
+})
+
+
+app.post("/velka", async (req, res) => {
+  if (!("velallinen" in req.body)) return res.status(400).send("Missing velallinen")
+  if (!("määrä" in req.body)) return res.status(400).send("Missing määrä")
+  if (!("maksettu" in req.body)) return res.status(400).send("Missing maksettu")
+  if (typeof req.body.velallinen !== "string") return res.status(400).send("velallinen must be string")
+  if (typeof req.body.määrä !== "number") return res.status(400).send("määrä must be number")
+  if (typeof req.body.maksettu !== "boolean") return res.status(400).send("maksettu must be boolean")
+  const velka = new Velka({velallinen: req.body.velallinen, määrä: req.body.määrä, maksettu: req.body.maksettu})
+  try {
+    await velka.save()
+    res.json(await Velka.find({}))
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("failed to save")
+  }
+})
+
+
+// Example:
+// curl -X POST -d '{"velallinen": "Aku Ankka", "määrä": 9000, "maksettu": false}' -H "Content-type: application/json"  localhost:4000/velka
+
+
+app.get("/velka", async (req, res) => {
+  try {
+    res.json(await Velka.find({}))
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("failed to get velat")
   }
 })
 
